65181b53f5bd499c26aaa5451ed6e46a803f67b1
guidobonomini <bono_9475@hotmail.com>
2017-10-20 02:09:48 -0300

    goalcoach - Initialize project with Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBIQhkdMN61q2fqockKJBnXbYOCn8hGGLQ",
+    authDomain: "goalcoach-6ef7d.firebaseapp.com",
+    databaseURL: "https://goalcoach-6ef7d.firebaseio.com",
+    projectId: "goalcoach-6ef7d",
+    storageBucket: "",
+    messagingSenderId: "1023952880516"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
